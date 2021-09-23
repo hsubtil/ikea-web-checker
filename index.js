5a3ef109d4bf02f995600b59e365a202cd4d8f09
Hugo SUBTIL <ext.sopra.husubtil@grandlyon.com>
2021-09-23 17:23:27 +0200

    feat: first version

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const checker = require('ikea-availability-checker');
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
@@ -6,5 +7,12 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/', async (req, res) => {
+    const storeId= '562'
+    const items = ['60342656', '60263244', '10488769', '80346941', '30277959', '60446545', '00295549', '30246722', '20348896', '50433987']
+    var data = await Promise.all(items.map(async (item) => {
+      return await checker.availability(storeId, item);
+    }))
+    res.render('pages/index', {data})
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
