364e20d6701a0487dd717288efb807ec9563e099
Hugo SUBTIL <ext.sopra.husubtil@grandlyon.com>
2024-07-23 21:07:40 +0200

    fix: clean code and update stock

--- index.js
+++ index.js
@@ -1,45 +1,46 @@
-const express = require('express')
-const checker = require('ikea-availability-checker');
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require("express");
+const checker = require("ikea-availability-checker");
+const path = require("path");
+const PORT = process.env.PORT || 5000;
+
+const PARILLY_STORE = "562";
+
+const getStockItems = async (items) =>
+  await Promise.all(
+    items.map(async (stockItem) =>
+      checker.availability(PARILLY_STORE, stockItem.id).then((item) => {
+        if (item) {
+          stockItem.name = item.stockItem;
+          return item;
+        }
+      })
+    )
+  );
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', async (req, res) => {
-    const storeId= '562'
-    const items = [ 
-    {id:'50214560', name: 'Pax 100x235'},
-    {id: '80329197', name:'Tyssedal Porte'},
-    {id: '20338411', name:'Porte - Poignées'},
-    {id: '30214504', name:'Porte - Charnières'},
-    {id: '30404028', name:'Tiroir bijoux'},
-    {id: '20246708', name:'Tiroir face en verre'},
-    {id: '70246386', name:'Plateau coulissant'},
-    {id: '70257638', name:'Tablettes en verre'},
-    {id: '70273092', name:'Norraryd chaises'},
-    {id: 's99390948', name:'Hemnes Lit + matelas'},
-    {id: 's19019226', name:'MALM Cadre lit, haut+4rgt'},
-  ]
-    var data = await Promise.all(items.map(async (item) => {
-      const tmp = await checker.availability(storeId, item.id);
-      tmp['name'] = item.name
-      return tmp
-    }))
-    res.render('pages/index', {data})
+  .use(express.static(path.join(__dirname, "public")))
+  .set("views", path.join(__dirname, "views"))
+  .set("view engine", "ejs")
+  .get("/", async (req, res) => {
+    const items = [
+      {
+        id: "s99530265",
+        name: "MITTZON - Bureau 160",
+      },
+      {
+        id: "00314741",
+        name: "KOPPLA - Prises",
+      },
+    ];
+    var data = await getStockItems(items);
+    res.render("pages/index", { data });
   })
-  .get('/hugo', async (req, res) => {
-    const storeId= '562'
-    const items = [ 
-    {id: '90506458', name: 'Bribri black'},
-    {id: '10503656', name: 'Bribri blanc'},
-  ]
-    var data = await Promise.all(items.map(async (item) => {
-      const tmp = await checker.availability(storeId, item.id);
-      tmp['name'] = item.name
-      return tmp
-    }))
-    res.render('pages/hugo', {data})
+  .get("/hugo", async (req, res) => {
+    const items = [
+      { id: "90506458", name: "Bribri black" },
+      { id: "10503656", name: "Bribri blanc" },
+    ];
+    var data = await getStockItems(items);
+    res.render("pages/hugo", { data: cleanData });
   })
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
