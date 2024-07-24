f0195f6813bc838449f43c55671b50e619447d06
Hugo SUBTIL <hugo.subtil@soprasteria.com>
2024-07-24 11:51:39 +0200

    feat: update responsive config

--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,9 @@ export default function Home() {
   const { items } = useStockItems();
   
   return (
-    <main className="flex min-h-screen flex-col items-center gap-8 p-24">
+    <main className="flex min-h-screen flex-col items-center gap-8 p-4 lg:p-16 xl:p-24">
       <h1 className="text-2xl font-bold tracking-tight text-gray-90">Ikea Web Checker</h1>
-      <div className="flex flex-col items-center gap-2 w-3/5"> 
+      <div className="flex flex-col items-center gap-2 w-4/5 lg:w-full"> 
       {
         items?.map((item) => item ? <ItemCard key={item.productId} data={item}></ItemCard> : <></>
         )
